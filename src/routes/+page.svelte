9609581bfc71e34eb7ac1aed6eacfb5b92fb18f7
Grant Wiswell <wiswellgrant@gmail.com>
2022-12-21 17:00:14 -0600

    Added title to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,11 @@
 	import VerticalTabs from "$components/navigation/VerticalTabs.svelte";
 
 </script>
+
+<svelte:head>
+	<title>Home | Grant Wiswell - Software Engineer </title>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
